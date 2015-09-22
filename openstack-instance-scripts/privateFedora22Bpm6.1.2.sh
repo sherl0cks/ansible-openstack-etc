93f5e6e9eed865b16ab9461cfdc39fe6f9f6367b
Justin Holmes <justinmichaelholmes@gmail.com>
2015-09-21 22:36:32 -0400

    incremental update

--- openstack-instance-scripts/privateFedora22Bpm6.1.2.sh
+++ openstack-instance-scripts/privateFedora22Bpm6.1.2.sh
@@ -9,4 +9,4 @@ INSTANCE_ID="jholmes-fedora22-bpm-6.1.2-"$(date +%Y-%m-%d-%T)
 echo "creating instance id: " $INSTANCE_ID
 
 # public OS1
-nova boot --flavor m1.medium --image 82cb0cb0-0c94-4396-888b-40873edca22e --key-name jholmes --security-groups bpms-group $INSTANCE_ID
+nova boot --flavor m1.medium --image 82cb0cb0-0c94-4396-888b-40873edca22e --key-name ba-enablement --security-groups bpms-group $INSTANCE_ID
